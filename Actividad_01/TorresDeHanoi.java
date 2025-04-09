d2e3de6a67f85de4b48f5a7f5d092d1b78755213
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-04-09 12:10:02 -0500

    TorresDeHaoni

--- Actividad_01/TorresDeHanoi.java
+++ Actividad_01/TorresDeHanoi.java
@@ -6,14 +6,14 @@ public class TorresDeHanoi {
     public static void torresHanoi(int discos, int torre1, int torre2, int torre3){
     //Caso base: mover un solo disco
         if(discos == 1){
-            System.out.println("Mover disco de torre"+" a torre " + torre3);
+            System.out.println("Mover disco de torre "+ torre1 +" a torre " + torre3);
             movimientos++ ; // Incremento de cantidad de movimientos
         } else  {
             //Mover n-1 discos de la torre 1 a la torre 2, se usa torre 3 como auxiliar 
             torresHanoi(discos-1, torre1, torre3, torre2);
         
             //Mover disco más grande de la torre 1 a torre 3
-            System.out.println("Moviendo disco de torre"+ torre1+"a torre"+ torre3);
+            System.out.println("Moviendo disco de torre "+ torre1+" a torre "+ torre3);
             movimientos++ ; // Incremento de cantidad de movimientos
 
             //Mover n-1 discos de la torre 2 a la torre 3, se usa torre 1 cmo auxiliar
