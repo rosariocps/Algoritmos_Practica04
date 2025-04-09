595c87572d038b708529a300644fed33f7047188
KahoriDiaz <kahori.diaz@estudiante.ucsm.edu.pe>
2025-04-08 20:28:49 -0500

    Pruebas de la recursividad

--- Actividad_01/PrincipalPrueba.java
+++ Actividad_01/PrincipalPrueba.java
@@ -5,12 +5,13 @@ public class PrincipalPrueba {
     public static void main(String[] args) {
         Scanner scannerr = new Scanner(System.in);
 
+        //Ingreso de cantidad de discos
         System.out.print("Ingrese el número de discos: ");
         int numDiscos = scannerr.nextInt();
-
+        // S reinicia el contador de movimientos antes de empezar 
         TorresDeHanoi.resetearMovimientos(); // Limpiar antes de iniciar
         TorresDeHanoi.torresHanoi(numDiscos, 1, 2, 3);
-
+        //Total de movimientos
         System.out.println("\nTotal de movimientos: " + TorresDeHanoi.obtenerMovimientos());
     }
 }
