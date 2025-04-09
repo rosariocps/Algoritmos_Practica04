6018081039716fd9efeef9f7aeef82fb669cd547
KahoriDiaz <kahori.diaz@estudiante.ucsm.edu.pe>
2025-04-08 22:46:11 -0500

    Prueba de método recursivo

---
+++ Actividad_03/PruebaRecursiva.java
@@ -0,0 +1,20 @@
+public class PruebaRecursiva {
+    public static void main(String[] args) {
+        BinarySearchRecursivo buscador = new BinarySearchRecursivo();
+
+        // Arreglo ordenado
+        int[] arreglo = {1, 2, 3, 4, 5};
+
+        // Número que queremos buscar
+        int valorBuscado = 4;
+
+        // Llamamos al método recursivo pasando los índices inicial y final
+        int posicion = buscador.binarySearch(arreglo, 0, arreglo.length - 1, valorBuscado);
+
+        // Mostramos el resultado
+        if (posicion == -1)
+            System.out.println("Elemento no encontrado.");
+        else
+            System.out.println("Elemento encontrado en el índice: " + posicion);
+    }
+}
